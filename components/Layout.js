85fc49790b9f5a84774be25d3eacdb869c3f94a8
minhtam3010 <minhtam30102002@gmail.com>
2023-08-25 19:38:49 +0700

    First Portfolio

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import { Sora } from "@next/font/google";
+
+// Font settings
+
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weights: [100, 200, 300, 400, 500, 600, 700, 800],
+});
+
+// Components
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImg from "../components/TopLeftImg";
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+    >
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
